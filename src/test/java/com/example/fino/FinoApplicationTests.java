bcaf09454f6737a5217f98afa9489a0994c258e4
박주원 <qkrwndnjs7308@gmail.com>
2024-07-16 15:28:43 +0900

    login

---
+++ src/test/java/com/example/fino/FinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
