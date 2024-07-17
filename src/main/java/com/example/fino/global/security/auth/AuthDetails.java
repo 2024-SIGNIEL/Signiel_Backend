6e2df815a1305c1c1dc3a4404a2735a2661f0ae1
박주원 <qkrwndnjs7308@gmail.com>
2024-07-17 09:55:33 +0900

    오류 수정

--- src/main/java/com/example/fino/global/security/auth/AuthDetails.java
+++ src/main/java/com/example/fino/global/security/auth/AuthDetails.java
@@ -2,11 +2,10 @@ package com.example.fino.global.security.auth;
 
 
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.Collections;
+
 
 public class AuthDetails implements UserDetails {
 
